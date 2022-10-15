4e81613da71991d5f3a9748275202e917a095d7d
obadahalak <obadahalak538@gmail.com>
2022-10-15 10:23:39 +0300

    userLocations

---
+++ database/migrations/2022_10_15_071634_create_user_locations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_locations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->point('lat');
+            $table->point('long');
+            $table->text('location');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_locations');
+    }
+};
